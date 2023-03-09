e45bdbfc14c06f63427380a121b6513a08dff2c3
jsh-1235 <jsh-1235@hanmail.net>
2023-03-09 17:41:24 +0900

    update

---
+++ memo.md
@@ -0,0 +1,55 @@
+# firebase
+
+- npm install -g firebase-tools
+- firebase --version
+- firebase projects:list
+
+## authentication
+
+- firebase login --reauth
+- firebase logout
+- firebase login
+- firebase projects:list
+
+## deploy
+
+- firebase init
+- firebase deploy
+- firebase serve
+
+## .firebaserc
+
+```json
+{
+  "projects": {
+    "default": "react-firebase-tutorial-c25dc"
+  }
+}
+```
+
+## firebase.json
+
+```json
+{
+  "hosting": {
+    "public": "build",
+    "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
+    "rewrites": [
+      {
+        "source": "**",
+        "destination": "/index.html"
+      }
+    ]
+  }
+}
+```
+
+## package.json
+
+```json
+{
+  "scripts": {
+    "deploy": "react-scripts build && firebase deploy"
+  }
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtKiJR4uiP-b_EBpEZ57e3ksYyNnPY11o",
+  authDomain: "react-firebase-tutorial-c25dc.firebaseapp.com",
+  projectId: "react-firebase-tutorial-c25dc",
+  storageBucket: "react-firebase-tutorial-c25dc.appspot.com",
+  messagingSenderId: "419671723562",
+  appId: "1:419671723562:web:cbbd29b44fa5e7c216fd53",
+  measurementId: "G-7Z01VLVLBM",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const fireStore = getFirestore(app);
 
 function App() {
+  const [topics, setTopics] = useState([]);
+
+  const read = async () => {
+    const querySnapshot = await getDocs(collection(fireStore, "topics"));
+
+    const items = [];
+
+    querySnapshot.forEach((document) => {
+      console.log(`${document.id} => ${document.data()}`);
+
+      items.push({ id: document.id, ...document.data() });
+    });
+
+    // console.log(items);
+
+    setTopics(items);
+  };
+
+  const add = async () => {
+    try {
+      const docRef = await addDoc(collection(fireStore, "topics"), {
+        title: "css",
+        description: "css is ...",
+      });
+
+      console.log(docRef.id);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleAdd = (e) => {
+    console.log(e.target);
+
+    add();
+
+    read();
+  };
+
+  useEffect(() => {
+    console.log(fireStore);
+
+    read();
+  }, []);
+
+  useEffect(() => {
+    // console.table(topics);
+
+    console.log(topics);
+  }, [topics]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleAdd}>add</button>
+      <ul>
+        {topics.map((topic) => {
+          return (
+            <li key={topic.id}>
+              <div>{topic.id}</div>
+              <div>{topic.title}</div>
+              <div>{topic.description}</div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
